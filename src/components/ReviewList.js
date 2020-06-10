ffcfd7558dfc4acbe475e5e4da2b1118b1615082
ylcarolcui <yiling_1212@hotmail.com>
2020-06-10 12:20:22 -0400

    changed styling for searchResult

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -30,37 +30,31 @@ function ReviewList(props) {
   // if (!reviews) return null;
   console.log(reviews)
 	return (
-			<div className='review'>
-				{filteredReview.map((review) => {
-					return (
-						<Container className='container-fluid d-flex justify-content-center '>
-							<Row>
-								<Col
-									sm={true}
-									className='text-center col-lg-6 col-sm-12 '>
-									<Card style={{ width: '18rem' }} className='single-review'>
-										<Card.Body>
-											<Card.Title>Review:</Card.Title>
-											<Card.Text>Posted by: {review.name}</Card.Text>
-											<Card.Text>description: {review.description}</Card.Text>
-											<Card.Text>overall: {review.overall_rating}</Card.Text>
-											<Card.Text>bedside: {review.bed_side_rating}</Card.Text>
-											<Card.Text>
-												wait time: {review.wait_time_rating}
-											</Card.Text>
-											<Card.Text>
-												post created at: {review.created_at}
-											</Card.Text>
-											<Card.Link href='#'>Edit</Card.Link>
-											<Card.Link href='#'>Delete</Card.Link>
-										</Card.Body>
-									</Card>
-								</Col>
-							</Row>
-						</Container>
-					);
-				})}
-			</div>
+		<div>
+			<Container className='container-fluid d-flex justify-content-center '>
+				<Row>
+					{filteredReview.map((review) => {
+						return (
+							<Col sm={true} className='mr-3'>
+								<Card style={{ width: '18rem' }} className='text-center h-100'>
+									<Card.Body>
+										<Card.Title>Review:</Card.Title>
+										<Card.Text>Posted by: {review.name}</Card.Text>
+										<Card.Text>description: {review.description}</Card.Text>
+										<Card.Text>overall: {review.overall_rating}</Card.Text>
+										<Card.Text>bedside: {review.bed_side_rating}</Card.Text>
+										<Card.Text>wait time: {review.wait_time_rating}</Card.Text>
+										<Card.Text>post created at: {review.created_at}</Card.Text>
+										<Card.Link href='#'>Edit</Card.Link>
+										<Card.Link href='#'>Delete</Card.Link>
+									</Card.Body>
+								</Card>
+							</Col>
+						);
+					})}
+				</Row>
+			</Container>
+		</div>
 	);
 }
 export default ReviewList;
--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {ListGroup, Card, CardDeck} from 'react-bootstrap';
+import {ListGroup, Card, CardDeck, Container} from 'react-bootstrap';
 import '../App.css';
 
 function SearchResult(props) {
@@ -11,26 +11,32 @@ function SearchResult(props) {
 	}
 	return (
 		<div>
-			{props.doctors.map((doctor) => {
-				return (
-					<div className='result-container'>
-						<CardDeck className='show-doctors'>
-							<Card>
-								<Card.Img variant='top' src={doctor.image_url} className='card-image'/>
-								<Card.Body>
-									<Card.Title>{doctor.first_name} {doctor.last_name}</Card.Title>
-									<Card.Text>{doctor.office_name}</Card.Text>
-								</Card.Body>
-								<Card.Footer>
-									<Link to={`/doctor/${doctor.id}`}>
-										<button>view</button>
-									</Link>
-								</Card.Footer>
-							</Card>
-						</CardDeck>
-					</div>
-				);
-			})}
+			<div className='result-container'>
+					{props.doctors.map((doctor) => {
+						return (
+							<CardDeck className='show-doctors'>
+								<Card className='text-center'>
+									<Card.Img
+										variant='top'
+										src={doctor.image_url}
+										className='card-image'
+									/>
+									<Card.Body>
+										<Card.Title>
+											{doctor.first_name} {doctor.last_name}
+										</Card.Title>
+										<Card.Text>{doctor.office_name}</Card.Text>
+									</Card.Body>
+									<Card.Footer>
+										<Link to={`/doctor/${doctor.id}`}>
+											<button>view</button>
+										</Link>
+									</Card.Footer>
+								</Card>
+							</CardDeck>
+						);
+					})}
+			</div>
 		</div>
 	);
 }
